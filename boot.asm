90a68d851b7d952e96d3044ca9edfc3d56736dc2
Anthony Roussel <anthony.roussel17@gmail.com>
2018-09-25 08:58:37 +0200

    document

--- boot.asm
+++ boot.asm
@@ -1,16 +1,23 @@
 [org 0x7c00]
+  ; print booting message
   mov bx, BOOTING_MSG
   call print_string
 
+  ; wait indefinitely
   jmp $
 
+; data
+; ====
 BOOTING_MSG:
   ; db stands for "declare bytes"
   ; and write directly into binary output file
   db 'Booting OS...', 0
 
+; file inclusions
+; ===============
 %include "print_string.asm"
 
 ; bios magic number
+; =================
 times 510-($-$$) db 0
 dw 0xaa55
--- print_string.asm
+++ print_string.asm
@@ -1,11 +1,20 @@
 print_string:
   pusha
+
+  ; int=10 / ah=0x0e -> BIOS tele-type output
   mov ah, 0x0e
 
 loop:
+  ; copy content at bx address in al
   mov al, [bx]
+
+  ; print the character in al
   int 0x10
+
+  ; next bx address
   add bx, 1
+
+  ; stop if al equals \0
   cmp al, 0
   jne loop
 
